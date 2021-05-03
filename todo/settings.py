c6366de8bcffd21817277e14035f4554c4e0199d
Tlotliso Mofokeng <tlotliso.mofokeng@yahoo.com>
2021-05-03 20:03:23 +0200

    settings Updated Commit

--- todo/settings.py
+++ todo/settings.py
@@ -126,8 +126,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
 
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
